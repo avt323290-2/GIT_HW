745866a55b8e5e53d62ab802f32864904f39678c
Smirnov Mihail <118544468+avt323290-2@users.noreply.github.com>
2023-12-08 22:57:58 +0300

    Update main.java

--- hw1/main.java
+++ hw1/main.java
@@ -3,6 +3,5 @@ package hw1;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Remote Repository!");
->>>>>>> db787ac27a4e988efc6dc8dc6ac6c231f7aae1de
     }
 }
